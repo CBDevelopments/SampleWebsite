0cec5a585c338596bf7f5da5b98ef7e25d92f553
CBDevelopments <71907197+CBDevelopments@users.noreply.github.com>
2020-10-03 16:46:24 +1000

    Final website

--- src/components/Pages/Home.js
+++ src/components/Pages/Home.js
@@ -2,12 +2,14 @@ import React from "react";
 import "../../styles.css";
 import HeroSection from "../HeroSection";
 import Cards from "../Cards";
+import Footer from "../Footer";
 
 function Home() {
   return (
     <>
       <HeroSection />
       <Cards />
+      <Footer />
     </>
   );
 }
